a7ae1c930e9f4d45d85222bb51ec0260b70cc50a
LeonYew <learngenius@outlook.com>
2025-04-02 09:43:31 +0800

    提交一些cpp的例子，更新gitignore

---
+++ ratio.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int width = 20;
+    int length = 30;
+
+    float ratio = width / length;
+
+    std::cout << ratio << "\n";
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+// 写一个hello world
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!222" << std::endl;
+    return 0;
+}
+ 
